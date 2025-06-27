83bae752b7b2dbfb725219cac8134d987d10f04e
stagiaire <hungreman400@gmail.com>
2025-06-27 10:33:04 +0200

    giant update added much better physics of ball added random ball bounce after louse added ball fixing to padle after you lost

--- main.py
+++ main.py
@@ -1,163 +1,138 @@
 import pygame
+import random
+import math
 
 pygame.init()
 scr = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Break Breaker')
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 72)
 FPS = 60
-speed = 12
 
-WIDTH = 800
-HEIGHT = 600
-
-# Loading assets
+#assets
 bg = pygame.image.load("./assets/images/bg.png")
 ball_img = pygame.image.load('assets/images/ball.png')
 paddle_img = pygame.image.load('assets/images/paddle.png')
-rectangle_img = pygame.image.load('assets/images/rectangle.png')
 
-ambient_sound = pygame.mixer.Sound('assets/sounds/ambient_sound.mp3')
-touched_bar_sound = pygame.mixer.Sound('assets/sounds/touched_bar.mp3')
+rectangle_img = pygame.image.load('assets/images/rectangle.png')
+touched_bar_sound   = pygame.mixer.Sound('assets/sounds/touched_bar.mp3')
 touched_brick_sound = pygame.mixer.Sound('assets/sounds/touched_brick.mp3')
 
-# === Paddle Class ===
+# Generate brick
+brick_rects = []
+rows, cols = 4, 11
+x0, y0 = 50, 50
+x_placing = rectangle_img.get_width()
+y_placing = rectangle_img.get_height()
+for row in range(rows):
+    for col in range(cols):
+        x = x0 + col * x_placing
+        y = y0 + row * y_placing
+        brick_rects.append(rectangle_img.get_rect(topleft=(x, y)))
+
+# Paddle
 class Paddle:
-    def __init__(self, x, y):
+    def __init__(self, x, y, speed):
         self.image = paddle_img
         self.rect = self.image.get_rect(topleft=(x, y))
         self.speed = speed
-
     def move(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self.rect.left > 0:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT] and self.rect.right < WIDTH:
-            self.rect.x += self.speed
+        if keys[pygame.K_LEFT]  and self.rect.left  > 0:       self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.rect.right < 800:     self.rect.x += self.speed
+    def draw(self):
+        scr.blit(self.image, self.rect.topleft)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect.topleft)
-
-# === Ball Class ===
+# Ball
 class Ball:
-    def __init__(self, posx, posy, radius, speed):
-        self.posx = posx
-        self.posy = posy
-        self.radius = radius
+    def __init__(self, x, y, speed):
+        self.image = ball_img
+        self.rect = self.image.get_rect(center=(x, y))
         self.speed = speed
-        self.xFac = 1
-        self.yFac = -1
-        self.rect = ball_img.get_rect(center=(posx, posy))
+        self.vx = 0  # Initialize velocities
+        self.vy = 0
+        self.active = True
+        self.reset_position(x, y)
+
+    def reset_position(self, x, y):
+        # Called only on initial spawn
+        self.rect.center = (x, y)
+        angle = random.uniform(-0.5, 0.5)  # slight horizontal angle in radians
+        self.vx = self.speed * math.sin(angle)
+        self.vy = -self.speed * math.cos(angle)
+
+    def reset(self, paddle):
+        self.rect.center = (paddle.rect.centerx, paddle.rect.top - self.rect.height)
+        self.active = False
+        angle = random.uniform(-0.5, 0.5)
+        self.vx = self.speed * math.sin(angle)
+        self.vy = -self.speed * math.cos(angle)
 
     def update(self, paddle):
-        # Move the ball
-        self.posx += self.speed * self.xFac
-        self.posy += self.speed * self.yFac
-        self.rect.center = (self.posx, self.posy)
+        if not self.active:
+            self.rect.centerx = paddle.rect.centerx
+            return
 
-        # Bounce off top
-        if self.posy - self.radius <= 0:
-            self.yFac *= -1
+        self.rect.x += int(self.vx)
+        self.rect.y += int(self.vy)
 
-        # Fell off bottom after reset
-        if self.posy - self.radius > HEIGHT:
-            self.reset()
+        # Wall collision
+        if self.rect.left <= 0 or self.rect.right >= 800:
+            self.vx *= -1
+        if self.rect.top <= 0:
+            self.vy *= -1
 
-        # Bounce off left/right walls
-        if self.posx - self.radius <= 0 or self.posx + self.radius >= WIDTH:
-            self.xFac *= -1
+        # Bottom of screen = reset
+        if self.rect.top > 600:
+            self.reset(paddle)
 
-        # Bounce off paddle
-        if self.yFac > 0 and self.rect.colliderect(paddle.rect):
-            self.yFac *= -1
+        # Paddle collision
+        if self.vy > 0 and self.rect.colliderect(paddle.rect):
             touched_bar_sound.play()
+            hit_pos = (self.rect.centerx - paddle.rect.centerx) / (paddle.rect.width / 2)
+            max_angle = 60
+            angle = hit_pos * math.radians(max_angle)
+            self.vx = self.speed * math.sin(angle)
+            self.vy = -self.speed * math.cos(angle)
 
-    def reset(self):
-        self.posx = WIDTH // 2
-        self.posy = HEIGHT // 2
-        self.xFac *= -1
-
-    def draw(self, surface):
-        surface.blit(ball_img, self.rect)
+    def draw(self):
+        scr.blit(self.image, self.rect.topleft)
 
 
-# Create paddle instance
-paddle = Paddle(x=WIDTH // 2 - paddle_img.get_width() // 2, y=HEIGHT - 50)
-ball = Ball(posx=400, posy=300, radius=10, speed=5)
+#Our configs of game
+paddle = Paddle( (800 - paddle_img.get_width())//2, 550, speed=20 )
+ball   = Ball(400, 300, speed=10)
 
-# === Game Loop ===
+
 running = True
 while running:
     clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
             running = False
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_SPACE and not ball.active:
+                ball.active = True
 
     scr.blit(bg, (0, 0))
-
-    # Updating and drawing objects
     paddle.move()
-    paddle.draw(scr)
-
+    paddle.draw()
     ball.update(paddle)
-    ball.draw(scr)
+    ball.draw()
 
-    # Draw of Briks     ((BRIK HEIGHT 30PX ====== BRIK WIDTH 63PX))
-    brik1 = scr.blit(rectangle_img, (50, 50))
-    brik5 = scr.blit(rectangle_img, (113, 50))
-    brik6 = scr.blit(rectangle_img, (176, 50))
-    brik7 = scr.blit(rectangle_img, (239, 50))
-    brik8 = scr.blit(rectangle_img, (302, 50))
-    brik9 = scr.blit(rectangle_img, (365, 50))
-    brik10 = scr.blit(rectangle_img, (428, 50))
-    brik11 = scr.blit(rectangle_img, (491, 50))
-    brik12 = scr.blit(rectangle_img, (554, 50))
-    brik13 = scr.blit(rectangle_img, (617, 50))
-    brik14 = scr.blit(rectangle_img, (680, 50))
+#brik breaking
+    if ball.active:
+        for rect in brick_rects[:]:
+            if ball.rect.colliderect(rect):
+                brick_rects.remove(rect)
+                touched_brick_sound.play()
+                ball.vy *= -1
+                break
 
-#####################################################################
+    # Draw bricks
+    for rect in brick_rects:
+        scr.blit(rectangle_img, rect.topleft)
 
-    brik2 = scr.blit(rectangle_img, (50, 80))
-    brik15 = scr.blit(rectangle_img, (113, 80))
-    brik16 = scr.blit(rectangle_img, (176, 80))
-    brik17= scr.blit(rectangle_img, (239, 80))
-    brik18 = scr.blit(rectangle_img, (302, 80))
-    brik19 = scr.blit(rectangle_img, (365, 80))
-    brik20 = scr.blit(rectangle_img, (428, 80))
-    brik21 = scr.blit(rectangle_img, (491, 80))
-    brik22 = scr.blit(rectangle_img, (554, 80))
-    brik23 = scr.blit(rectangle_img, (617, 80))
-    brik24 = scr.blit(rectangle_img, (680, 80))
-
-#####################################################################
-
-    brik3 = scr.blit(rectangle_img, (50, 110))
-    brik25 = scr.blit(rectangle_img, (113, 110))
-    brik26 = scr.blit(rectangle_img, (176, 110))
-    brik27 = scr.blit(rectangle_img, (239, 110))
-    brik28 = scr.blit(rectangle_img, (302, 110))
-    brik29 = scr.blit(rectangle_img, (365, 110))
-    brik30 = scr.blit(rectangle_img, (428, 110))
-    brik31 = scr.blit(rectangle_img, (491, 110))
-    brik32 = scr.blit(rectangle_img, (554, 110))
-    brik33 = scr.blit(rectangle_img, (617, 110))
-    brik34 = scr.blit(rectangle_img, (680, 110))
-
-#####################################################################
-
-    brik4 = scr.blit(rectangle_img, (50, 140))
-    brik35 = scr.blit(rectangle_img, (113, 140))
-    brik36 = scr.blit(rectangle_img, (176, 140))
-    brik37 = scr.blit(rectangle_img, (239, 140))
-    brik38 = scr.blit(rectangle_img, (302, 140))
-    brik39 = scr.blit(rectangle_img, (365, 140))
-    brik40 = scr.blit(rectangle_img, (428, 140))
-    brik41 = scr.blit(rectangle_img, (491, 140))
-    brik42 = scr.blit(rectangle_img, (554, 140))
-    brik43 = scr.blit(rectangle_img, (617, 140))
-    brik44 = scr.blit(rectangle_img, (680, 140))
-
-#####################################################################
-
-    pygame.display.update()
+    pygame.display.flip()
 
 pygame.quit()
